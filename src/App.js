df26dfdd5098f977cd9619198cb0b45a3f2ac697
jjanmo <jjanmo07@hanmail.net>
2021-05-24 23:46:08 +0900

    Refactor structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AppList from './components/AppList';
+
+function App() {
+  return (
+    <div>
+      <h1>크고 작은 프로그램(앱) 리스트 뿌려집니다.!!</h1>
+      <AppList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AppList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AppList = () => {
+  return '리스트입니다.🤬';
+};
+
+export default AppList;
