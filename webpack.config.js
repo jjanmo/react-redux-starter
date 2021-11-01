57f0ca2e57900258aaf7c12e7f0c750267bd89a8
jjanmo <jjanmo07@hanmail.net>
2021-11-01 23:45:10 +0900

    feat: add  feature of reading env file in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,47 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const dotenv = require('dotenv');
+const webpack = require('webpack');
 
-module.exports = {
-  mode: 'development',
-  entry: ['./src/index.js'],
-  output: {
-    path: path.resolve('./dist'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        exclude: [/node_modules/, require.resolve('./index.html')],
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
+module.exports = () => {
+  const env = dotenv.config().parsed;
+
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+  }, {});
+
+  return {
+    mode: 'development',
+    entry: ['./src/index.js'],
+    output: {
+      path: path.resolve('./dist'),
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          exclude: [/node_modules/, require.resolve('./index.html')],
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
           },
         },
-      },
+      ],
+    },
+    node: {
+      fs: 'empty',
+    },
+    plugins: [
+      new HTMLWebpackPlugin({
+        template: 'index.html',
+      }),
+      new webpack.DefinePlugin(envKeys),
     ],
-  },
-  plugins: [
-    new HTMLWebpackPlugin({
-      template: 'index.html',
-    }),
-  ],
-  devServer: {
-    hot: 'only',
-  },
+    devServer: {
+      hot: 'only',
+    },
+  };
 };
